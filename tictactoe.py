41bd844cec025e8504303a6172cd4c2c7672bc81
Ryan Gamboa <gabe.gamboa05@gmail.com>
2024-05-14 19:13:50 -0700

    adding printBoard function to tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -3,4 +3,13 @@ theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
             'low-L': ' ', 'low-M': ' ', 'low-R': ' ',
             }
 
-print(theBoard)
+#printBoard function
+def printBoard(board):
+    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'] + '|')
+    print('-+-+-')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'] + '|')
+    print('-+-+-')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'] + '|')
+
+#calling printBoard function by passing theBoard as arg val 
+printBoard(theBoard)
