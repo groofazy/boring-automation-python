317a512da558fa16d5affa15dcfd0c06d45bf438
Ryan Gamboa <gabe.gamboa05@gmail.com>
2024-05-14 19:43:03 -0700

    Finished tictactoe.py project

--- tictactoe.py
+++ tictactoe.py
@@ -5,11 +5,23 @@ theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
 
 #printBoard function
 def printBoard(board):
-    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'] + '|')
+    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
     print('-+-+-')
-    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'] + '|')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
     print('-+-+-')
-    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'] + '|')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+
+turn = 'X'
+# for loop which updates the board after every turn, for a max of 9 turns for 9 spaces
+for i in range(9):
+    printBoard(theBoard)
+    print("Turn for " + turn + ". Move on which space?")
+    move = input()
+    theBoard[move] = turn
+    if turn == 'X':
+        turn = 'O'
+    else:
+        turn = 'X'
 
 #calling printBoard function by passing theBoard as arg val 
 printBoard(theBoard)
